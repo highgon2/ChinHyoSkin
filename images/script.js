e0b9eadafc6b3b90ef7fc332f33ce7ab28f96976
gon <highgon2@gmail.com>
2022-01-14 22:00:14 +0900

    initial commit  - version 2.4

---
+++ images/script.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+    $('.footer-inner').append('<p class="copyleft">Copyleft &copy; <a id="copyleft" href="https://rgy0409.tistory.com" target="_blank">친절한효자손</a><a id="skinver" href="https://rgy0409.tistory.com/3119" target="_blank">(친효스킨 v2.4)</a></p>');
+});
